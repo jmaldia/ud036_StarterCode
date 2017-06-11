edaeb5d84c0eb0bc7288569af5ddafe1e83b2c4a
Jon Maldia <jon@maldia.net>
2017-06-10 22:16:06 -0400

    Updated media.py with docstring

--- media.py
+++ media.py
@@ -1,8 +1,9 @@
-# This file defines the class Movie that contains the attributes that a movie object needs. It helps stores movie related information. 
+# This file defines the class Movie that contains the attributes that a movie object needs. 
 
 class Movie():
-	# Constructor for Movie class. Initializes the variables needed to instantiate an object. 
-	def __init__(self, title, poster_image_url, trailer_youtube_url):
+	"""This class helps store movie related information.""" 
+
+	def __init__(self, title, poster_image_url, trailer_youtube_url):  # Constructor for Movie class. Initializes the variables needed to instantiate an object.
 		self.title = title
 		self.poster_image_url = poster_image_url
 		self.trailer_youtube_url = trailer_youtube_url
